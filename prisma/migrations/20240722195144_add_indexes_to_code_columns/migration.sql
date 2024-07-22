4e27f60ffdf9fd2a822b300fae617c75f25dc9be
alexandrtovmach <alexandrtovmach@gmail.com>
2024-07-22 23:19:43 +0200

    feat: Added indexes

---
+++ prisma/migrations/20240722195144_add_indexes_to_code_columns/migration.sql
@@ -0,0 +1,11 @@
+-- CreateIndex
+CREATE INDEX "archives_code_idx" ON "archives"("code");
+
+-- CreateIndex
+CREATE INDEX "cases_code_idx" ON "cases"("code");
+
+-- CreateIndex
+CREATE INDEX "descriptions_code_idx" ON "descriptions"("code");
+
+-- CreateIndex
+CREATE INDEX "funds_code_idx" ON "funds"("code");
