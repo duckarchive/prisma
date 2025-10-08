0417264ce05c698a9df7bfbf060c791ac53fc4b5
alexandrtovmach <alexandrtovmach@gmail.com>
2025-10-08 11:43:01 +0200

    fix: Disabled all limits for text col on fs items table

---
+++ prisma/inspector/migrations/20251008094249_extend_all_text_cols/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "family_search_items" ALTER COLUMN "volume" SET DATA TYPE TEXT,
+ALTER COLUMN "volumes" SET DATA TYPE TEXT,
+ALTER COLUMN "archival_reference" SET DATA TYPE TEXT;
