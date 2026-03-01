212f7db1664023ac0a7b87729ef6258a31e27d50
Alexandr Tovmach <alexandrtovmach@gmail.com>
2026-03-01 03:01:34 +0100

    feat: Added indexes

---
+++ prisma/inspector/migrations/20260301014500_add_indexes/migration.sql
@@ -0,0 +1,27 @@
+-- CreateIndex
+CREATE INDEX "funds_archive_id_idx" ON "public"."funds"("archive_id" ASC);
+
+-- CreateIndex
+CREATE INDEX "descriptions_fund_id_idx" ON "public"."descriptions"("fund_id" ASC);
+
+-- CreateIndex
+CREATE INDEX "cases_description_id_idx" ON "public"."cases"("description_id" ASC);
+
+-- CreateIndex
+CREATE INDEX "cases_info_idx" ON "public"."cases" USING GIN ("info" gin_trgm_ops ASC);
+
+-- CreateIndex
+CREATE INDEX "cases_tags_idx" ON "public"."cases" USING GIN ("tags" array_ops ASC);
+
+-- CreateIndex
+CREATE INDEX "cases_title_idx" ON "public"."cases" USING GIN ("title" gin_trgm_ops ASC);
+
+-- CreateIndex
+CREATE INDEX "case_online_copies_case_id_idx" ON "public"."case_online_copies"("case_id" ASC);
+
+-- CreateIndex
+CREATE INDEX "authors_title_idx" ON "public"."authors" USING GIN ("title" gin_trgm_ops ASC);
+
+-- CreateIndex
+CREATE INDEX "case_authors_author_id_idx" ON "public"."case_authors"("author_id" ASC);
+
