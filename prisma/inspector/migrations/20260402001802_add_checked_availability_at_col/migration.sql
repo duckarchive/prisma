ff2ba9119db31e63854662374ff39ea0a01f2b89
Alexandr Tovmach <alexandrtovmach@gmail.com>
2026-04-02 02:18:20 +0200

    feat: Added checked_at col

---
+++ prisma/inspector/migrations/20260402001802_add_checked_availability_at_col/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "case_online_copies" ADD COLUMN     "checked_availability_at" TIMESTAMP(6);
+
+-- AlterTable
+ALTER TABLE "description_online_copies" ADD COLUMN     "checked_availability_at" TIMESTAMP(6);
